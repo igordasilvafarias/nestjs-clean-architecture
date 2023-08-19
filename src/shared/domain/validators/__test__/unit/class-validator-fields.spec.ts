4a2d18e592d114e84c14f9c93f90bfdccfaf5d91
Igor Farias <igordasilvafarias@gmail.com>
2023-08-19 17:35:54 -0300

    feat: unit test for validator fields

--- src/shared/domain/validators/__test__/unit/class-validator-fields.spec.ts
+++ src/shared/domain/validators/__test__/unit/class-validator-fields.spec.ts
@@ -27,4 +27,14 @@ describe('ClassValidatorFields unit tests', () => {
     expect(sut.validatedData).toBeNull();
     expect(sut.errors).toStrictEqual({ field: ['test error'] });
   });
+  it('should validate without errors', () => {
+    const sut = new StubClassValidatorFields();
+    const spyValidateSync = jest.spyOn(libClassValidator, 'validateSync');
+    spyValidateSync.mockReturnValue([]);
+
+    expect(sut.validate({ field: 'value' })).toBeTruthy();
+    expect(spyValidateSync).toHaveBeenCalled();
+    expect(sut.validatedData).toStrictEqual({ field: 'value' });
+    expect(sut.errors).toBeNull();
+  });
 });
